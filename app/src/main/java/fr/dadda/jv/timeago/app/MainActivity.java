180cfdd4b206eddd3dd8e02ed0602274c01cb4ba
Jean-Vincent D'Adda <jvdadda@accengage.com>
2017-03-10 18:16:57 +0100

    Initialize project

---
+++ app/src/main/java/fr/dadda/jv/timeago/app/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.dadda.jv.timeago.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(fr.dadda.jv.timeago.app.R.layout.activity_main);
+    }
+}
